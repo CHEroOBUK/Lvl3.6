0e4c980945467ce774548256922c4a081805cdf5
CHEroOBUK <Sapsan_90@mail.ru>
2020-08-08 13:03:04 +0300

    ДЗ 3.6

--- src/main/java/SomeMethods.java
+++ src/main/java/SomeMethods.java
@@ -1,2 +1,32 @@
 public class SomeMethods {
+
+    int[] getArrayPastLast4(int[] array) throws RuntimeException{
+        int posLast4 = -1;
+        int[] newArray;
+        for (int i = array.length - 1; i >= 0; i--){
+            if (array[i] == 4) {
+                posLast4 = i;
+                break;
+            }
+        }
+        if (posLast4 == -1){
+            throw new RuntimeException("В массиве нет значения \"4\"");
+        }
+        newArray = new int[array.length-posLast4-1];
+        for (int i = 0; i < newArray.length; i++){
+            newArray[i] = array[posLast4+i+1];
+        }
+        return newArray;
+    }
+
+    boolean is1and4exist(int[] array){
+        boolean is1Exist = false;
+        boolean is4Exist = false;
+        for (int a : array) {
+            if (a == 1 && !is1Exist) is1Exist = true;
+            if (a == 4 && !is4Exist) is4Exist = true;
+        }
+        return is1Exist && is4Exist;
+    }
+
 }
--- src/test/java/ParanoidTester.java
+++ src/test/java/ParanoidTester.java
@@ -1,2 +1,59 @@
+import org.junit.*;
+
 public class ParanoidTester {
+
+    SomeMethods someMethods;
+
+    @Before
+    public void init(){
+        System.out.println("--init SomeMethods--");
+        someMethods = new SomeMethods();
+    }
+
+    @Test
+    public void method1_test1(){
+        Assert.assertArrayEquals(new int[]{1, 2, 3}, someMethods.getArrayPastLast4(new int[]{4, 1, 2, 3}));
+    }
+
+    /*@Test(expected = RuntimeException.class)
+    public void method1_test2(){
+        someMethods.getArrayPastLast4(new int[]{0, 1, 2, 3});
+    }*/
+
+    @Test
+    public void method1_test2() throws RuntimeException{
+        try {
+            someMethods.getArrayPastLast4(new int[]{0, 1, 2, 3});
+            Assert.fail("Должно быть исключение!");
+        } catch (RuntimeException e){
+            Assert.assertEquals("В массиве нет значения \"4\"", e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void method1_test3(){
+        Assert.assertArrayEquals(new int[]{}, someMethods.getArrayPastLast4(new int[]{4, 1, 2, 3, 4}));
+    }
+
+    @Test
+    public void method2_test1(){
+        Assert.assertTrue(someMethods.is1and4exist(new int[]{4, 1, 2, 3, 4}));
+    }
+
+    @Test
+    public void method2_test2(){
+        Assert.assertFalse(someMethods.is1and4exist(new int[]{4, 7, 8, 3, 4}));
+    }
+
+    @Test
+    public void method2_test3(){
+        Assert.assertTrue(someMethods.is1and4exist(new int[]{1, 4, 2, 3, 4}));
+    }
+
+    @After
+    public void shutdown(){
+        System.out.println("--end test--");
+    }
+
 }
